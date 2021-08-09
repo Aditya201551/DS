06b71a5305004566e60ffe759ef6e50d23cb4eb9
Aditya Raj <aditya201551@gmail.com>
2021-08-09 15:03:49 +0530

    eafdqw

--- DThread.java
+++ DThread.java
@@ -4,7 +4,9 @@ public class DThread {
         t1.setDaemon(true);
         System.out.println(t1.isDaemon()+" "+t1.getPriority());
         t1.start();
-        new t("Not a daemon boi").start();
+        t t2=new t("Not a daemon boi");
+        t2.setDaemon(true);
+        t2.start();
     }
 }
 
@@ -15,6 +17,22 @@ class t extends Thread{
     }
     public void run()
     {
-        System.out.println("Finished: "+currentThread().getName()+" "+currentThread().getPriority());
+        if(isDaemon()) {
+            try {
+                sleep(5000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        else{
+            try {
+                sleep(3000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Finished: "+getName()+" "+getPriority());
     }
 }
