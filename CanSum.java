e8af80db72fe3605b5f2faa6234d403bd3cebb93
Aditya Raj <aditya201551@gmail.com>
2021-09-20 21:08:16 +0530

    added DP code

---
+++ CanSum.java
@@ -0,0 +1,75 @@
+import java.util.*;
+public class CanSum {
+
+    // basic iteration with O(n^2)
+    boolean findSum(int arr[], int target)
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+        int sum = 0;
+
+            inner:
+           for(int j=i;j<arr.length;j++)
+           {
+               sum+=arr[j];
+               if(sum>target)
+                    break inner;
+                if(sum==target)
+                    return true;
+           }
+        }
+
+        return false;
+    }
+
+    // basic recursion with O(n^m)
+    boolean findSum(int target, int arr[])
+    {
+        if(target==0)
+            return true;
+        // if(target<0)
+        //     return false;
+
+        for(int i:arr)
+        {
+            int tmp=target-i;
+            if(tmp>=0)
+                if(findSum(tmp, arr))
+                    return true;
+        }
+
+        return false;
+    }
+
+    // memoized method with O(m*n)
+    boolean findSum(int target, int arr[], HashMap<Integer, Boolean> memo)
+    {
+        if(memo.containsKey(target))
+            return memo.get(target);
+        if(target==0)
+            return true;
+        if(target<0)
+            return false;
+
+        for(int i : arr)
+        {
+            int tmp=target-i;
+            if(tmp>=0)
+                if(findSum(tmp, arr, memo)){
+                    memo.put(target, true);
+                    return true;
+                }
+        }
+
+        memo.put(target, false);
+        return false;
+    }
+
+    public static void main(String[] args) {
+        CanSum ob=new CanSum();
+        System.out.println(ob.findSum(new int[]{50,150}, 100));
+        System.out.println(ob.findSum(21, new int[] {7,14}));
+        System.out.println(ob.findSum(300, new int[] { 7,14 }, new HashMap<>()));
+
+    }
+}
--- GridTravel.java
+++ GridTravel.java
@@ -1,6 +1,8 @@
+import java.util.HashMap;
+
 public class GridTravel {
     
-    int travel(int m, int n)
+    long travel(long m, long n)
     {
         if(m==1||n==1)
             return 1;
@@ -10,9 +12,31 @@ public class GridTravel {
         return travel(m-1, n)+travel(m, n-1);
     }
 
+    long travel(long m, long n, HashMap<String, Long> memo)
+    {
+        String key=m+","+n;
+        String key2=n+","+m;
+
+        if(memo.containsKey(key)||memo.containsKey(key2))
+            return memo.get(key)==null?memo.get(key2):memo.get(key);
+        if(m==0||n==0)
+            return 0;
+        if(m==1||n==1)
+            return 1;
+
+        memo.put(key,(travel(m-1, n,memo)+travel(m, n-1,memo)));
+
+        return memo.get(key);
+    }
+
     public static void main(String[] args) {
         GridTravel ob=new GridTravel();
         
-        System.out.println(ob.travel(2,3));
+        System.out.println(ob.travel(2,2));
+        // System.out.println(ob.travel(100, 100, new HashMap<String, Long>()));
+
+        // int a=15,b=7;
+        // String s=a+","+b;
+        // System.out.println(s);
     }
 }
