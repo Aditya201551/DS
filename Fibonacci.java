51aa33c2228a51c8c3e6a882a7b63e8471a0d3fe
CrispyWrecker <aditya201551@gmail.com>
2021-09-25 21:42:08 +0530

    allconstruct

--- Fibonacci.java
+++ Fibonacci.java
@@ -23,12 +23,24 @@ public class Fibonacci {
         return memo.get(n);
     }
 
+    //tabulation
+    long fibo(int n)
+    {
+        long arr[]=new long[n+1];
+        arr[0]=1;arr[1]=1;
+        for(int i=2;i<n;i++)
+            arr[i]=arr[i-2]+arr[i-1];
+        return arr[n-1];
+    }
+
     public static void main(String args[])
     {
         Fibonacci ob=new Fibonacci();
 
-        System.out.println(ob.fib(4));
+        System.out.println(ob.fib(9));
 
         System.out.println(ob.fib(999, new HashMap<Long, Long>()));
+
+        System.out.println(ob.fibo(0));
     }
 }
