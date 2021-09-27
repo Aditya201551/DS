292a4bbce4bf49ec6ac4e8559f73b3b72d3674da
CrispyWrecker <aditya201551@gmail.com>
2021-09-27 22:20:34 +0530

    tabulation

--- HowSum.java
+++ HowSum.java
@@ -45,10 +45,45 @@ public class HowSum {
         return null;
     }
 
+    // tabulation: time: O((m^2)*n); space: O(m^2)
+    ArrayList<Integer> howSum(int target, int numbers[])
+    {
+        ArrayList<ArrayList<Integer>> list=new ArrayList<>();
+        for(int i=0;i<=target;i++)
+            list.add(null);
+
+            list.set(0, new ArrayList<>());
+            
+            for(int i=0;i<=target;i++)
+            {
+                if(list.get(i)!=null)
+                {
+                    for(int j:numbers)
+                        if(i+j<list.size())
+                        {
+                            if(list.get(i+j)==null||list.get(i).size()<list.get(i+j).size())// to get the best shortest sum from the array
+                            {
+                                list.set(i+j, (ArrayList)list.get(i).clone());
+                                list.get(i+j).add(j);
+                            }
+                        }
+                }
+                if(list.get(target)!=null)
+                    return list.get(target);
+            }
+
+        return null;
+        
+    }
+
     public static void main(String[] args) {
         HowSum ob=new HowSum();
         // System.out.println(ob.findSum(300, new int[]{7,14}));
-        System.out.println(ob.findSum(100, new int[]{90,96,4}, new HashMap<>()));
+        // System.out.println(ob.findSum(100, new int[]{90,96,4}, new HashMap<>()));
+        for(int i=0;i<=20;i++)
+        System.out.println(ob.howSum(i, new int[]{3,4,5}));
+
+        System.out.println(ob.howSum(100, new int[]{1,3,25}));
 
         //! Side Note on .equals() function
         // ArrayList<Integer> a,b;
