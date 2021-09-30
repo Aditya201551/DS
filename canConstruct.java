e21cfe9e8f80c8b78e8defeb4e6a2d520fce51ab
CrispyWrecker <aditya201551@gmail.com>
2021-09-30 15:51:15 +0530

    countConstruct

--- canConstruct.java
+++ canConstruct.java
@@ -37,11 +37,35 @@ public class canConstruct {
         memo.put(word, false);
         return false;
     }
+
+    //tabulation
+    boolean CanConstruct(String target, String arr[])
+    {
+        boolean result[]=new boolean[target.length()+1];
+        result[0]=true;
+
+        for(int i=0;i<=target.length();i++)
+        {
+            if(result[i])
+                for(String word:arr)
+                    if(i+word.length()<=target.length())
+                        if(target.substring(i, i+word.length()).equals(word))
+                            result[i+word.length()]=true;
+            if(result[target.length()])
+                return true;
+        }
+
+    return false;
+    }
     public static void main(String[] args) {
         canConstruct ob=new canConstruct();
         
-        System.out.println(ob.checkWord("abcdef", new String[]{"ab","abc","cd","def","abcd"}, new HashMap<>()));
-        System.out.println(ob.checkWord("skateboard", new String[] {"bo","rd","ate","t","ska","sk","boar"}, new HashMap<>()));
-        System.out.println(ob.checkWord("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", new String[] { "e", "ee", "eee", "eeee", "eeeee" }, new HashMap<>()));
+        // System.out.println(ob.checkWord("abcdef", new String[]{"ab","abc","cd","def","abcd"}, new HashMap<>()));
+        // System.out.println(ob.checkWord("skateboard", new String[] {"bo","rd","ate","t","ska","sk","boar"}, new HashMap<>()));
+        // System.out.println(ob.checkWord("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", new String[] { "e", "ee", "eee", "eeee", "eeeee" }, new HashMap<>()));
+
+        System.out.println(ob.CanConstruct("abcdef", new String[]{"ab","abc","cd","def","abcd"}));
+        System.out.println(ob.CanConstruct("skateboard", new String[] {"bo","rd","ate","t","ska","sk","e","boar"}));
+        System.out.println(ob.CanConstruct("enterpotentpot", new String[]{"a","p","ent","enter","ot","o","t"}));
     }
 }
--- countConstruct.java
+++ countConstruct.java
@@ -22,9 +22,35 @@ public class countConstruct {
         return totalWays;
     }
 
+    int countWays(String target, String arr[])
+    {
+        int result[]=new int[target.length()+1];
+        result[0]=1;
+
+        for(int i=0;i<=target.length();i++){
+            if(result[i]!=0){
+                for(String word:arr){
+                    if(i+word.length()<=target.length()){
+                        if(target.substring(i,i+word.length()).equals(word)){
+                            result[i+word.length()]+=result[i];
+
+                            //! DEBUGGER
+                            // System.out.println(Arrays.toString(result));
+                        }
+                    }
+                }
+            }
+        }
+         
+        return result[target.length()];
+    }
+
     public static void main(String[] args) {
         countConstruct ob=new countConstruct();
-        System.out.println(ob.countWays("enterapotentpot", new String[]{"a","p","ent","enter","ot","o","t"}, new HashMap<>()));
-        System.out.println(ob.countWays("purple", new String[]{"p","purp","ur","le","purpl"}, new HashMap<>()));
+        // System.out.println(ob.countWays("enterapotentpot", new String[]{"a","p","ent","enter","ot","o","t"}, new HashMap<>()));
+        // System.out.println(ob.countWays("purple", new String[]{"p","purp","ur","le","purpl"}, new HashMap<>()));
+
+        System.out.println(ob.countWays("enterapotentpot", new String[]{"a","p","ent","enter","ot","o","t"}));
+        System.out.println(ob.countWays("purple", new String[]{"p","purp","ur","le","purpl"}));
     }
 }
